076faac8c7f1abde62cda3726b5df5e70082d0b7
Fernando1403 <fernandogalexandre@hotmail.com>
2024-09-10 20:13:49 -0300

    First Commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import MovieListPage from './pages/MovieListPage.jsx'
+import MovieDetailPage from './pages/MovieDetailPage.jsx'
+import GenreListPage from './pages/GenreListPage.jsx'
+import MovieByGenrePage from './pages/MoviesByGenrePage.jsx'
+import PageNotFound from './pages/PageNotFound.jsx'
 
 {/* Deve ter as rotas:
   home
@@ -12,8 +19,23 @@ import './index.css'
   Page Not Found
   */}
 
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <App/>,
+      children: [
+        {index: true, element: <Home />},
+        {path: 'movie', element: <MovieListPage />},
+        {path: 'movieDetail', element: <MovieDetailPage />},
+        {path: 'genreListPage', element: <GenreListPage />},
+        {path: 'movieByGenrePage', element: <MovieByGenrePage />},
+        {path: 'pageNotFound', element: <PageNotFound />}
+      ]
+    }
+])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
